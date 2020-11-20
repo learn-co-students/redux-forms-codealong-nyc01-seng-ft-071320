ea9badb75267e46f904c00703a3907fd8245d995
humorous-storage-7521 <rmwerner@umich.edu>
2020-11-20 14:03:35 -0600

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,14 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      console.log('state changed')
+      return {
+        todos: [...state.todos, action.payload.todo]
+      }
+  
+    default:
+      return state;
+  }
 }
